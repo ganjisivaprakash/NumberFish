81bac695c6f397a7181ccd015e4496e7a83268b4
Hari Krishnan <rajahari1996@gmail.com>
2016-07-17 10:15:38 +0530

    updated

--- app/src/main/java/in/devzone/numberfish/MainActivity.java
+++ app/src/main/java/in/devzone/numberfish/MainActivity.java
@@ -1,11 +1,21 @@
 package in.devzone.numberfish;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,22 +35,49 @@ import java.net.URLConnection;
 /**
  * Created by hari on 10/7/16.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemSelectedListener{
     public String PhoneNum;
+    public String Code;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_layout);
-        Button findBtn = (Button) findViewById(R.id.findButton);
-        findBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView numField = (TextView) findViewById(R.id.editText);
-                PhoneNum = numField.getText().toString();
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.country_list,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+            Button findBtn = (Button) findViewById(R.id.findButton);
+            findBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    TextView numField = (TextView) findViewById(R.id.editText);
+                    PhoneNum = numField.getText().toString();
+                    if (PhoneNum.isEmpty()) {
+                        TextView alertField = (TextView) findViewById(R.id.alertView);
+                        alertField.setText("Please Enter Number");
+                    } else {
+                        new GetData().execute(PhoneNum);
+                    }
+                }
+            });
 
-                new GetData().execute(PhoneNum);
-            }
-        });
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent ,View view,int position,long id){
+        String code = parent.getItemAtPosition(position).toString();
+        Code = code;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Code = "+91";
+    }
+    public boolean isNetAvailable(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetInfo != null && activeNetInfo.isConnected();
     }
     private class GetData extends AsyncTask<String, String, String>{
         String data;
@@ -58,7 +95,9 @@ public class MainActivity extends Activity {
                 if(respCode == httpURLConnection.HTTP_OK){
                     InputStream dataStream = new BufferedInputStream(httpURLConnection.getInputStream());;
                     data = convertInputStreamToString(dataStream);
-
+                }
+                else{
+                    return "nothing";
                 }
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -71,19 +110,78 @@ public class MainActivity extends Activity {
 
         }
         protected void onPostExecute(String get){
-            try {
-                JSONObject jsonObject = new JSONObject(get.toString());
-                String number = jsonObject.getString("message");
-                String status = jsonObject.getString("status");
-                setContentView(R.layout.result_layout);
-                TextView numField = (TextView) findViewById(R.id.textView);
-                numField.setText(number);
-                TextView statusField = (TextView) findViewById(R.id.textView2);
-                statusField.setText(status);
+                try {
+                    JSONObject jsonObject = new JSONObject(get.toString());
+                    String number = jsonObject.getString("number");
+                    String status = jsonObject.getString("status");
+                    setContentView(R.layout.result_layout);
+                    Button backBtn = (Button) findViewById(R.id.backButton);
+                    backBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            setContentView(R.layout.main_activity_layout);
+                        }
+                    });
+                    if (status.equals("0")) {
+                        String recycled = jsonObject.getString("recycled");
+                        String date = jsonObject.getString("date");
+                        TextView numField = (TextView) findViewById(R.id.numberView);
+                        numField.setText(number);
+                        TextView statusField = (TextView) findViewById(R.id.statusView);
+                        statusField.setText("In Active");
+                        TextView dateField = (TextView) findViewById(R.id.dateView);
+                        dateField.setText(date);
+                        if (recycled.equals("0")) {
+                            TextView rdateField = (TextView) findViewById(R.id.rdateView);
+                            rdateField.setText("Not Recycled");
+                        } else if (recycled.equals("1")) {
+                            String r_date = jsonObject.getString("r_date");
+                            TextView rdateField = (TextView) findViewById(R.id.rdateView);
+                            rdateField.setText("Recycled on");
+                            TextView rdateDateField = (TextView) findViewById(R.id.rdateDateView);
+                            rdateDateField.setText(r_date);
+                        }
+                    } else if (status.equals("1")) {
+                        String recycled = jsonObject.getString("recycled");
+                        String date = jsonObject.getString("date");
+                        TextView numField = (TextView) findViewById(R.id.numberView);
+                        numField.setText(number);
+                        TextView statusField = (TextView) findViewById(R.id.statusView);
+                        statusField.setText("Active");
+                        TextView dateField = (TextView) findViewById(R.id.dateView);
+                        dateField.setText(date);
+                        if (recycled.equals("0")) {
+                            TextView rdateField = (TextView) findViewById(R.id.rdateView);
+                            rdateField.setText("Not Recycled");
+                        } else if (recycled.equals("1")) {
+                            String r_date = jsonObject.getString("r_date");
+                            TextView rdateField = (TextView) findViewById(R.id.rdateView);
+                            rdateField.setText("Recycled on");
+                            TextView rdateDateField = (TextView) findViewById(R.id.rdateDateView);
+                            rdateDateField.setText(r_date);
+                        }
 
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+                    } else if (status.equals("2")) {
+                        TextView numField = (TextView) findViewById(R.id.numberView);
+                        numField.setText("Number Unavailable");
+                        TextView stextField = (TextView) findViewById(R.id.statusTextView);
+                        stextField.setVisibility(View.INVISIBLE);
+                        TextView rtextField = (TextView) findViewById(R.id.rdateTextView);
+                        rtextField.setVisibility(View.INVISIBLE);
+                    } else {
+                        TextView numField = (TextView) findViewById(R.id.numberView);
+                        numField.setText("Unexpected Error!");
+                        TextView stextField = (TextView) findViewById(R.id.statusTextView);
+                        stextField.setVisibility(View.INVISIBLE);
+                        TextView rtextField = (TextView) findViewById(R.id.rdateTextView);
+                        rtextField.setVisibility(View.INVISIBLE);
+                    }
+                } catch (JSONException e) {
+                    TextView numField = (TextView) findViewById(R.id.numberView);
+                    numField.setText("Connection Unavailable!");
+                    e.printStackTrace();
+                }
+
 
         }
         private String convertInputStreamToString(InputStream inputStream) throws IOException {
